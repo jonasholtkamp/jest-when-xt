45d0fd9d0c9c92212e7d870c34baacc8cad5afa7
Jonas Holtkamp <jonas.holtkamp@senacor.com>
2020-03-16 12:43:04 +0100

    Security patches

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,12 +1,10 @@
-module.exports = function (config) {
-  config.set({
-    testRunner: 'jest',
-    testFramework: 'jest',
-    mutator: { name: 'javascript', excludedMutations: ['StringLiteral'] },
-    transpilers: [],
-    reporters: ['clear-text', 'progress', 'html'],
-    coverageAnalysis: 'off',
-    mutate: ['src/**/*.js', '!src/**/*.test.js', '!src/**/*.testdata.js'],
-    maxConcurrentTestRunners: 4
-  })
+module.exports = {
+  testRunner: 'jest',
+  testFramework: 'jest',
+  mutator: { name: 'javascript', excludedMutations: ['StringLiteral'] },
+  transpilers: [],
+  reporters: ['clear-text', 'progress', 'html'],
+  coverageAnalysis: 'off',
+  mutate: ['src/**/*.js', '!src/**/*.test.js'],
+  maxConcurrentTestRunners: 4
 }
