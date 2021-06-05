cd8d866cfe199176f4d4b65d551f3c5ebee05e8a
Jonas Holtkamp <jonas.holtkamp@senacor.com>
2021-06-05 12:34:05 +0200

    Upgrade to jest@27

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,7 +2,6 @@ module.exports = {
   testRunner: 'jest',
   mutator: { excludedMutations: ['StringLiteral'] },
   reporters: ['clear-text', 'progress', 'html'],
-  coverageAnalysis: 'off',
   mutate: ['src/**/*.js', '!src/**/*.test.js'],
   concurrency: 4
 }
